f85a85a4f6be50b0e28a7ee0ad2f9d0ff9f13e9d
ximo <ximo.gil14@gmail.com>
2020-12-02 15:00:35 +0100

    add state to localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -12,8 +12,32 @@ export default new Vuex.Store({
     },
     mutations: {
         setIsLoggedIn(state, payload) {
+
             state.user.isLoggedIn = payload.isLoggedIn;
+
             state.user.email = state.user.isLoggedIn ? payload.email : '';
+
+            if (state.user.isLoggedIn) {
+
+                const parsed = JSON.stringify(this.state.user);
+
+                localStorage.setItem('user', parsed);
+
+            } else {
+
+                if (localStorage.getItem('user')) {
+
+                    try {
+
+                        localStorage.removeItem('user');
+
+                    } catch (error) {
+
+                        localStorage.removeItem('user');
+
+                    }
+                }
+            }
         }
     },
     actions: {
